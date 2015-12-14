5be194e1013d68d22cabe226b303e764f92f504c
James Skarzinskas <james@jskarzin.org>
2015-12-14 02:37:02 -0500

    :art: style, viewport

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const HTML = `
     <head>
         <meta charset="utf-8">
         <title>React WebSocket Chat Example</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="/bundle.js" type="text/javascript"></script>
     </head>
 
